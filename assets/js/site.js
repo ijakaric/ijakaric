c67ae37e217466b42a0cdfcc45773f4a63661ff0
root <r00t@roots-MacBook-Pro.local>
2020-02-12 15:49:32 +0100

    google tag

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,3 +7,19 @@ jQuery(function($) {
 	const sliders = require('./_site/sliders');
 	sliders.init();
 });
+
+  // Not a ton of code, but hard to
+  const nav = document.querySelector('.site-header');
+  let topOfNav = nav.offsetTop;
+
+  function fixNav() {
+	if (window.scrollY >= topOfNav) {
+	//   document.body.style.paddingTop = nav.offsetHeight + 'px';
+	  document.body.classList.add('fixed-nav');
+	} else {
+	  document.body.classList.remove('fixed-nav');
+	//   document.body.style.paddingTop = 0;
+	}
+  }
+
+  window.addEventListener('scroll', fixNav);
