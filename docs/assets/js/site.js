06ebb3cc66bc07dc3a7bb7939b99bbbdb72fa353
Igor Jakaric <igorjakaric@Igors-MacBook-Pro.local>
2021-01-15 15:27:35 +0100

    preload

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -46,3 +46,10 @@ jQuery(function($) {
 
   
   new WOW().init();
+
+  $(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
